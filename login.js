a82f6571057299d65c5c272b28b266a5d887185f
Beker-Dev <joaopedro.beker@hotmail.com>
2022-10-20 00:14:14 -0300

    .

--- login.js
+++ login.js
@@ -1,24 +1,55 @@
 import fakeFetchApi from "./utils/fakeFetchApi.js";
 
-
-var email = '';
-var password = '';
+var email;
+var password;
 
 
 function handleChange() {
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
-    
-    console.log(email, password, handleLogin())
+
+    if (isValidPassword()) {
+        handleLogin()
+    }
+    else {
+        addErrorMsg("Senha invalida", "passwordError", false)
+    }
+
 }
 
 function handleLogin() {
-    return fakeFetchApi({
+    var result = fakeFetchApi({
         "email": email,
         "password": password
     })
+
+    if (result.status == 200) {
+        window.location.href = "/app"
+    }
+    else {
+        addErrorMsg(result.message, "errorMsg")
+    }
 }
 
+function addErrorMsg(msg, tagId, useTimeout=true) {
+    document.getElementById(tagId).innerText = msg;
+    if (useTimeout) {
+        setTimeout(cleanErrorMsg, 2 * 1000, tagId)
+    }
+}
+
+function cleanErrorMsg(tagId) {
+    document.getElementById(tagId).innerHTML = null;
+}
+
+function isValidPassword() {
+    var regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/
+    if (password.length >= 8 && regex.test(password)) {
+        return true
+    }
+
+    return false
+}
 
 
 window.addEventListener('keyup', handleChange)
