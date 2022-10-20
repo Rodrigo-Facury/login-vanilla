2988e15fc68083d012cf88832aa0b78c28f05fc2
Eduardo Mendes <eduardompereira4@outlook.com>
2022-10-20 00:52:53 -0300

    add msg error, validation email and password & update html

--- login.js
+++ login.js
@@ -1,18 +1,53 @@
-function values(){
-    const username = document.getElementById("username")
-    const password = document.getElementById("password")
-    const loginButton = document.getElementById("login")
+import fakeFetchApi from "./utils/fakeFetchApi.js"
+
+var loginButton = document.getElementById("login")
+
+function handleChange() {
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    const user = {
+        email, 
+        password
+    }
+
+    const responseFakeApi = fakeFetchApi(user)
+
+    if(!validateEmailAndPassword(user.email, user.password)){
+       generateNotification("e-mail e senha em formatos inválidos")
+    }
+
+    if(responseFakeApi.status === 200){
+        window.location.href = "/app"
+    }else{
+        generateNotification(responseFakeApi.message)
+    }
+}
+
+
+function handleLogin() {
+    //loginButton.addEventListener("click", handleChange)
+    handleChange()
+}
+
+function generateNotification(message) {
+    document.getElementById("notification-error").innerText = message
+    setTimeout(clearNotification, 2 * 1000)
+}
+
+function clearNotification() {
+    document.getElementById("notification-error").innerHTML = null
+}
+
+function validateEmailAndPassword(email, password){
+
+    var regexPassword = /(?=.[a-z])(?=.[A-Z])(?=.*\W)/
+    var regerxEmail =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     
-    const user = {username, password}
-    console.log(user)
+    if (password.length >= 8 && regexPassword.test(password) && regerxEmail.test(email)) {
+        return true
+    }
+    return false
 }
 
-window.addEventListener("keyup", values)
-
-// window.onload = function handleChange() {
-//      loginButton.addEventListener('keyup', values)
-// }
-
-window.onload = function handleLogin() {
-
-}
+handleLogin()
